466670f425e99b4067c7d7319bfc683725b7c8a6
dhritiavala <davala@ucsc.edu>
2025-05-21 00:31:54 +0000

    added recipe_ingredient table to db

--- backend/apps/CustomRecipeManager/models.py
+++ backend/apps/CustomRecipeManager/models.py
@@ -19,31 +19,42 @@ from .common import Field, db, auth
 # ingredient table
 
 db.define_table(
-        'ingredient', 
+        'ingredient',
         Field('name', 'string', length=64, unique=True, requires=[IS_NOT_EMPTY(), IS_LENGTH(64)]),
-        Field('unit', 'string', requires=IS_IN_SET(['g', 'kg', 'ml', 'l', 'tsp', 'tbsp', 'cup', 'oz', 'piece', 'bunch','stick', 'slice', 'pinch', 'clove', 'can','jar', 'pack', 'sheet', 'head', 'leaf', 'filet', 'sprig'])), 
-        Field('description', 'text', requires=IS_LENGTH(500)), 
-        Field('calories_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('protein_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('fat_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('carbs_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('sugar_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('fiber_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        Field('sodium_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)), 
-        auth.signature, 
+        Field('unit', 'string', requires=IS_IN_SET(['g', 'kg', 'ml', 'l', 'tsp', 'tbsp', 'cup', 'oz', 'piece', 'bunch','stick', 'slice', 'pinch', 'clove', 'can','jar', 'pack', 'sheet', 'head', 'leaf', 'filet', 'sprig'])),
+        Field('description', 'text', requires=IS_LENGTH(500)),
+        Field('calories_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('protein_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('fat_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('carbs_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('sugar_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('fiber_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        Field('sodium_per_unit', 'double', requires=IS_FLOAT_IN_RANGE(0, 10000)),
+        auth.signature,
         format='%(name)s'
 )
 
 db.define_table(
-    'recipe',
+    'recipe', 
     Field('name', 'string', length=100, requires=[IS_NOT_EMPTY(), IS_LENGTH(100)]),
     Field('type', 'string', requires=IS_IN_SET(['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Dessert', 'Drink'])),
     Field('description', 'text', requires=IS_LENGTH(1000)),
     Field('image', 'upload'),
     Field('author', 'reference auth_user'),
     Field('instruction_steps', 'text', requires=IS_NOT_EMPTY()),
-    Field('servings', 'integer', requires=IS_INT_IN_RANGE(1, 100)), auth.signature, format='%(name)s'
+    Field('servings', 'integer', requires=IS_INT_IN_RANGE(1, 100)), 
+    auth.signature, 
+    format='%(name)s'
 )
 
+db.define_table(
+    'recipe_ingredient',
+    Field('recipe_id', 'reference recipe', requires=IS_NOT_EMPTY()),
+    Field('ingredient_id', 'reference ingredient', requires=IS_NOT_EMPTY()),
+    Field('quantity_per_serving', 'double', requires=IS_FLOAT_IN_RANGE(0.01, 10000)),
+    auth.signature, 
+    format='%(ingredient_id)s'
+)
 
-db.commit()        
+db.commit()
+
