dff7280538c10cdf128254e794d9a1fb217a2bb0
Trisha <trsriram@ucsc.edu>
2025-06-08 20:39:41 +0000

    fixed a slight bug in the init file

--- backend/apps/CustomRecipeManager/__init__.py
+++ backend/apps/CustomRecipeManager/__init__.py
@@ -5,7 +5,7 @@ import sys
 assert py4web.check_compatible("1.20190709.1")
 
 # by importing controllers you expose the actions defined in it
-if not any(x in sys.argv[0] for x in ["cleanup_themealdb.py", "some_other_script.py"]):
+if not any(x in sys.argv[0] for x in ["cleanup_themealdb.py", "import_themealdb.py"]):
     from . import controllers
 # by importing db you expose it to the _dashboard/dbadmin
 from .models import db
